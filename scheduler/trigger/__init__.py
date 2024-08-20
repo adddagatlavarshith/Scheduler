f1f31320dfb005c95eb727d30a84bfc8d07acad7
adddagatlavarshith <addagatlavarshith@gmail.com>
2024-08-20 14:30:20 +0530

    Trigger collection.

---
+++ scheduler/trigger/__init__.py
@@ -0,0 +1,16 @@
+"""
+Trigger collection.
+"""
+
+from scheduler.trigger.core import (
+    Friday,
+    Monday,
+    Saturday,
+    Sunday,
+    Thursday,
+    Tuesday,
+    Wednesday,
+    weekday,
+)
+
+__all__ = ["Friday", "Monday", "Saturday", "Sunday", "Thursday", "Tuesday", "Wednesday", "weekday"]
